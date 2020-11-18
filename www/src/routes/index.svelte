551db27c6d66adba5ee68f05580f270232916b48
Brian Ketelsen <mail@bjk.fyi>
2020-11-18 15:59:18 -0500

    strapi

--- www/src/routes/index.svelte
+++ www/src/routes/index.svelte
@@ -1,41 +1,23 @@
+<script context="module">
+  export const prerender = true;
+  export async function preload({ params }) {
+    const res = await this.fetch(
+      `http://content.44ec396da6024f08b75a.eastus2.aksapp.io/home`
+    );
+    const homepage = await res.json();
+    return { homepage };
+  }
+</script>
+
 <script>
-  import Counter from "$components/Counter.svelte";
-  import { onMount } from "svelte";
-  let homepage = {};
+  export let homepage;
   let error = null;
-
-  onMount(async () => {
-    const parseJSON = (resp) => (resp.json ? resp.json() : resp);
-    const checkStatus = (resp) => {
-      if (resp.status >= 200 && resp.status < 300) {
-        return resp;
-      }
-      return parseJSON(resp).then((resp) => {
-        throw resp;
-      });
-    };
-    const headers = {
-      "Content-Type": "application/json",
-    };
-
-    try {
-      const res = await fetch("http://localhost:1337/home", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then(checkStatus)
-        .then(parseJSON);
-      homepage = res;
-      console.log(homepage);
-    } catch (e) {
-      error = e;
-    }
-  });
 </script>
 
 <style>
 </style>
 
-{#if error !== null}{error}{:else}{homepage.title}{/if}
+<h1>{homepage.title}</h1>
+<img
+  src="http://content.44ec396da6024f08b75a.eastus2.aksapp.io{homepage.head_image.url}" />
+<div>{homepage.hero_text}</div>
