96123f16761f4f60196f1fc93fd65c3584074135
Brian Ketelsen <mail@bjk.fyi>
2020-11-20 12:35:37 -0500

    login

---
+++ www/src/routes/login/index.svelte
@@ -0,0 +1,25 @@
+<script>
+  async function handleClick(event) {
+    let response = await fetch("https://content.gophercon.com/auth/local", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      body: JSON.stringify({
+        identifier: "mail@bjk.fyi",
+        password: "testing123",
+      }),
+    });
+
+    let result = await response.json();
+    console.log(result);
+  }
+</script>
+
+<style>
+</style>
+
+<main>
+  <h1>Log In</h1>
+  <button on:click={handleClick}> log In </button>
+</main>
